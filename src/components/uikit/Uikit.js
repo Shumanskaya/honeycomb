211329b20c7161a91c4ee0b77c7e125e20cf816a
Evgenia <myemail@mail.ru>
2021-08-02 14:59:17 +0300

    создание проекта HoneyComb

---
+++ src/components/uikit/Uikit.js
@@ -0,0 +1,15 @@
+import Sidevar from "./Sidebar";
+import BottomMenu from "./BottomMenu";
+import GlobalStyle from "./GlobalStyle";
+
+function Uikit() {
+    return (
+        <div>
+            <Sidevar />
+            <BottomMenu />
+            <GlobalStyle />
+        </div>
+    )
+}
+
+export default Uikit
